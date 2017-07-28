6877aa879632cb185d3927ce14a5a1190c433c37
ruinalen <ruinalen@gmail.com>
2017-07-28 17:40:26 +0900

    ADD LOGIN

--- member/include/dbConnect.php
+++ member/include/dbConnect.php
@@ -1,7 +1,8 @@
 <?php
-define('DB_SERVER', 'localhost:3036');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'rtemd513');
-define('DB_DATABASE', 'madchicken');
-$dbConnect = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+    $host = 'localhost';
+    $user = 'root';
+    $passWord = 'rtemd513';
+    $dbName = 'madchicken';
+
+    $dbConnect = new mysqli($host,$user,$passWord,$dbName);
 ?>
