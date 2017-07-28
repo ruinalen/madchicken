4778f7d5fc628a7901b30f4585c51b9272f4dad7
ruinalen <ruinalen@gmail.com>
2017-07-28 15:29:20 +0900

    DATABASE RE EDIT

--- board/comment_update.php
+++ board/comment_update.php
@@ -1,5 +1,5 @@
 <?php
-require_once("./board/dbconfig.php");
+require_once("./dbconfig.php");
 $id = $_POST["id"];
 $writer = $_POST["writer"];
 $password = $_POST["password"];
--- board/view.php
+++ board/view.php
@@ -1,5 +1,5 @@
 <?php
-require_once("./board/dbconfig.php");
+require_once("./dbconfig.php");
 
 $id = $_GET["id"];
 
