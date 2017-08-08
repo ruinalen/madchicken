85ae22a3e7d5b85fc4720669ff7d466fbc8c3dae
ruinalen <ruinalen@gmail.com>
2017-08-08 14:22:02 +0900

    Edit Paging

--- newboard/del.php
+++ newboard/del.php
@@ -4,13 +4,13 @@ include "db_info.php";
 //파일업로드 관련 함수 파일
 include "library.php";
 
-$result=mysqli_query("select pass,filename from $board where id=$_GET[id]", $conn);
+$result=mysqli_query($conn,"select pass,filename from $board where id=$_GET[id]");
 $row=mysqli_fetch_array($result);
 
  if ($_POST[pass]==$row[pass] )
  {
 	 $conndel = "delete from $board where id=$_GET[id] ";
-	 $result=mysqli_query($conndel, $conn);
+	 $result=mysqli_query($conn,$conndel);
 
 	//첨부된 파일이 있으면 지운다.
 	 if ($row[filename]) del_file($row[filename]);
