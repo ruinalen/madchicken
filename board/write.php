2abbcd5d00414f739b765a62f46effa9a1427dca
ruinalen <ruinalen@gmail.com>
2017-08-08 11:36:09 +0900

    Edit Paging

--- board/write.php
+++ board/write.php
@@ -1,6 +1,52 @@
 <?php
 require_once("./dbconfig.php");
 
+$uploads_dir = "uploads/";
+$allowed_ext = array('jpg','jpeg','png','gif');
+
+// 변수 정리
+$error = $_FILES['myfile']['error'];
+$name = $_FILES['myfile']['name'];
+$ext = array_pop(explode('.', $name));
+
+// 오류 확인
+if( $error != UPLOAD_ERR_OK ) {
+    switch( $error ) {
+        case UPLOAD_ERR_INI_SIZE:
+        case UPLOAD_ERR_FORM_SIZE:
+            echo "파일이 너무 큽니다. ($error)";
+            break;
+        case UPLOAD_ERR_NO_FILE:
+            echo "파일이 첨부되지 않았습니다. ($error)";
+            break;
+        default:
+            echo "파일이 제대로 업로드되지 않았습니다. ($error)";
+    }
+    exit;
+}
+
+// 확장자 확인
+if( !in_array($ext, $allowed_ext) ) {
+    echo "허용되지 않는 확장자입니다.";
+    exit;
+}
+
+// 파일 이동
+move_uploaded_file( $_FILES['myfile']['tmp_name'], "$uploads_dir/$name");
+
+// 파일 정보 출력
+echo "<h2>파일 정보</h2>
+	<ul>
+		<li>파일명: $name</li>
+		<li>확장자: $ext</li>
+		<li>파일형식: {$_FILES['myfile']['type']}</li>
+		<li>파일크기: {$_FILES['myfile']['size']} 바이트</li>
+	</ul>";
+
+
+
+
+
 if (isset($_GET["id"]))
 {
     $id = $_GET["id"];
@@ -8,7 +54,9 @@ if (isset($_GET["id"]))
 
 if (isset($id))
 {
-    $sql = "select id, title, content, date, hit, writer, password from board where id=" . $id;
+    $sql = "select id, title, content, date, hit, writer, password, myfile from board where id=" . $id;
+
+
     $result = $db->query($sql);
     $row = $result->fetch_assoc();
 }
@@ -61,8 +109,8 @@ if (isset($id))
                 </tr>
                 <tr>
                     <th scope="row"><label for="title">FILE</label></th>
-                    <td class="title"><input type="file" name="title" id="file"
-                                             value="<?php echo isset($row["file"])?$row["file"]:null?>"></td>
+                    <td class="title"><input type="myfile" name="myfile" id="myfile"
+                                             value="<?php echo isset($row["myfile"])?$row["myfile"]:null?>"></td>
                 </tr>
                 </tbody>
             </table>
