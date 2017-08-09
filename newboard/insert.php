1b8777646327a00fa20d18eaebce2d22de0598a3
ruinalen <ruinalen@gmail.com>
2017-08-09 19:10:43 +0900

    Edit Paging

--- newboard/insert.php
+++ newboard/insert.php
@@ -11,7 +11,7 @@ $max_thread_fetch = mysqli_fetch_row($max_thread_result);
 $max_thread = ceil($max_thread_fetch[0]/1000)*1000+1000;
 
 //업로드 파일이 있으면 업로드 함수 호출 50*1024는 파일크기 제한 즉, 50KB이상은 업로드 불가
-if ($HTTP_POST_FILES[upfile][name]) $filename=upload($HTTP_POST_FILES[upfile],50*1024);
+if ($HTTP_POST_FILES[upfile][name]) $filename=upload($HTTP_POST_FILES[upfile],5000000*1024);
 echo "파일이름은 : "+$filename+"입니다";
 $query = "insert into $board values (0,$max_thread,0,'$_POST[name]','$_POST[pass]','$_POST[email]','$_POST[title]','$_POST[comment]',". time() .",'',0,'$filename',0)";
 echo $query;
