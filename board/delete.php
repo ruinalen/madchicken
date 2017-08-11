4abb8824ca509f5acd633101c73cb900ba4d6721
ruinalen <ruinalen@gmail.com>
2017-08-11 16:52:42 +0900

    Edit Paging

--- board/delete.php
+++ board/delete.php
@@ -1,107 +1,79 @@
-﻿<?
-include "lib/bm_dbconfig.php"; // DB 연결을 위한 환경 파일
-include "lib/bm_function.php"; // 사용자 정의 함수
+<?php
+require_once("./dbconfig.php");
 
-if($id == '') {
-	bm_error("게시판 이름이 지정되지 않았습니다.");
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
 }
-
-$connect = bm_dbconnect($host, $db_id, $db_passwd, $db_name); // MySQL DB 연결(사용자 정의 함수 bm_dbconnect 이용)
-$sql = "select * from multiconfig where id = '$id'" ; // 0000.php?id=xxx 와 같은 식으로 호출
-$result = mysql_query($sql) or die (mysql_error().$sql);
-$array = mysql_fetch_array($result);
-$skindir = "skin/".$array[skin]."/"; // 스킨 디렉토리
-$tablename = $id; // 테이블 이름
-$title = $array[title] ;//"다중 게시판" ;
-$head_file = $array[head_file];//게시판 상단 꾸미기
-$head = stripslashes($array[head]);
-$foot_file = $array[foot_file];//게시판 하단 꾸미기
-$foot = stripslashes($array[foot]);
-$tbwidth = $array[tbwidth];// 게시판 테이블 가로
-$tbalign = $array[tbalign];// 게시판 테이블 정렬 방식
-$background = $array[background]; // 전체 배경 그림
-$strlen = $array[strlen]; // 제목 글자수
-
-//테이블에서 글을 가져옵니다. 
-$query = "select * from $tablename where number='$number'"; // 글 번호를 가지고 조회를 합니다.
-$result = mysql_query($query) or die (mysql_error());
-$array = mysql_fetch_array($result);
-
-// 스킨에서 사용할 변수
-$a_ok = "<a href='javascript:check_submit();'>";
-$a_cancel = "<a href='javascript:history.go(-1)'>";
-
-if($HTTP_COOKIE_VARS[cook_userid] != '') { $hide_start="<!--"; $hide_end="-->"; } // 회원일 경우 가릴 때 사용
-
 ?>
-
-
-<? include "copyright.txt"; // 저작권 ?>
-
+<!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv=content-type content=text/html; charset=euc-kr>
-<title><?=$title;?></title>
-<? include $skindir."style.css"; ?>
-<?
-if (!$HTTP_COOKIE_VARS[cook_userid]) { // 로그인하지 않은 상태이면
-echo "
-<script language='javascript'>
-function check_submit() {
-	if (document.myForm.password.value == '') {
-		alert('비밀번호를 입력해야 글을 삭제할 수 있습니다.');
-		document.myForm.password.focus();
-		return;
-	} else {
-		document.myForm.action = 'delete_ok.php';
-		document.myForm.submit();
-	}
-}
-</script>
-";
-} else {
-echo "
-<script language='javascript'>
-function check_submit() {
-	document.myForm.action = 'delete_ok.php';
-	document.myForm.submit();
-}
-</script>
-";
-}
-?>
+    <meta charset="utf-8"/>
+    <title>Free Board</title>
+    <link rel="stylesheet" href="./css/normalize.css"/>
+    <link rel="stylesheet" href="./css/board.css"/>
 </head>
-<body leftmargin="0" topmargin="0" background="<?=$background; ?>">
+<body>
+<article class="boardArticle">
+    <h3>Post Delete</h3>
+    <?php
+    if (isset($id)) {
+        $sql = "select count(id) as cnt from board where id = '$id'";
+        $result = $db->query($sql);
+        $row = $result->fetch_assoc();
+    if (empty($row["cnt"])) {
+        ?>
+        <script>
+            alert("Do not exist");
+            history.go(-1);
+        </script>
+    <?php
+    exit;
+    }
 
-<? if ($head_file != '') include $head_file ; ?>
-<? echo "$head"; ?>
-
-<form name="myForm" method="get">
-<input type="hidden" name="id" value="<? echo $id; ?>">
-<input type="hidden" name="page" value="<? echo $page; ?>">
-<input type="hidden" name="number" value="<? echo $number; ?>">
-<input type=hidden name=src_name value='<? echo $src_name; ?>'>
-<input type=hidden name=src_value value='<? echo $src_value; ?>'>
-
-<table border=0 cellspacing=0 width=<?=$tbwidth;?> align=<?=$tbalign;?>>
-<tr><td>
-
-<!-- 여기서부터 스킨-->
-
-<? include $skindir."delete_main.php"; ?>
-
-<!-- 여기까지 스킨 -->
-
-<p align="right">
-<? include $skindir."copyright.php"; // 저작권?>
-</p>
-
-</td></tr>
-</table>
-
-</form>
-
-<? if ($foot_file != '') include $foot_file ; ?>
-<? echo "$foot"; ?>
+    $sql = "select title from board where id = '$id'";
+    $result = $db->query($sql);
+    $row = $result->fetch_assoc();
+    ?>
+        <div id="boardDelete">
+            <form action="./delete_update.php" method="post">
+                <input type="hidden" name="id" value="<?php echo $id ?>">
+                <table>
+                    <caption class="readHide">Post Delete</caption>
+                    <thead>
+                    <tr>
+                        <th scope="col" colspan="2">Delete</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row">title</th>
+                        <td><?php echo $row['title'] ?></td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><label for="password">password</label></th>
+                        <td><input type="password" name="password" id="password"></td>
+                    </tr>
+                    </tbody>
+                </table>
+                <div class="btnSet">
+                    <button type="submit" class="btnSubmit btn">delete</button>
+                    <a href="./index.php" class="btnList btn">return</a>
+                </div>
+            </form>
+        </div>
+    <?php
+    //if (isset($id)) {
+    } else {
+    ?>
+        <script>
+            alert('정상적인 경로를 이용해주세요.');
+            history.go(-1);
+        </script>
+        <?php
+        exit;
+    }
+    ?>
+</article>
 </body>
 </html>
--- board/skin/default/copyright.php
+++
@@ -1,3 +0,0 @@
-﻿<span style=font-size:8pt;><font face=Tahoma>copyleft <a href="http://www.itmembers.net"
-                                                         target="_blank">ITmembers.net</a> / skin by <a
-                href="http://www.itmembers.net" target="_blank">ITmembers.net</a></font></span>
