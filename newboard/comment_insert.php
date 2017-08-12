439a33e5d2301af4256487d9926ccac24fc6507a
ruinalen <ruinalen@gmail.com>
2017-08-12 16:04:10 +0900

    Edit Paging

--- newboard/comment_insert.php
+++ newboard/comment_insert.php
@@ -1,7 +1,27 @@
 ﻿<?
 include "db_info.php";
-?>
-<?
+
+if (!trim($HTTP_POST_VARS[comment])) {
+    ?>
+
+    <script>
+        alert("내용을 입력하여 주십시오.");
+        history.back(-1);
+    </script>
+    <?
+    exit;
+}
+
+// 간혹 이름이 넘어오지 않는 경우가 생김 (왜 그런지는 아직 모르겠음)
+if (!$name) {
+    ?>
+    <script>
+        alert("이름을 입력하여 주십시오.");
+        history.back(-1);
+    </script>
+    <?
+    exit;
+}
 //코멘트 등록
 $sql = " insert into comment VALUES ('','$bid','$name','$passwd','$comment',now(),'$REMOTE_ADDR')";
 $result = mysqli_query($conn,$sql);
